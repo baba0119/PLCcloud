b3bc825adb5b7f3a6d39322a79d2d383afedad27
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-09-17 14:26:16 +0900

    仮想GPIOのマッピング用構造体完成

---
+++ PLC/datamodel/vrgpiomodel/vrgpiomap.go
@@ -0,0 +1,39 @@
+package vrgpiomodel
+
+// 仮想gpioのマッピング用構造体
+// type VRgpioMap struct {
+// 	gpio0 	VRgpio
+// 	gpio1		VRgpio
+// 	gpio2		VRgpio
+// 	gpio3		VRgpio
+// 	gpio4		VRgpio
+// 	gpio5		VRgpio
+// 	gpio6		VRgpio
+// 	gpio7		VRgpio
+// 	gpio8		VRgpio
+// 	gpio9		VRgpio
+// 	gpio10 VRgpio
+// 	gpio11 VRgpio
+// 	gpio12 VRgpio
+// 	gpio13 VRgpio
+// 	gpio14 VRgpio
+// 	gpio15 VRgpio
+// 	gpio16 VRgpio
+// 	gpio17 VRgpio
+// 	gpio18 VRgpio
+// 	gpio19 VRgpio
+// 	gpio20 VRgpio
+// 	gpio21 VRgpio
+// 	gpio22 VRgpio
+// 	gpio23 VRgpio
+// 	gpio24 VRgpio
+// 	gpio25 VRgpio
+// 	gpio26 VRgpio
+// 	gpio27 VRgpio
+// 	gpio28 VRgpio
+// 	gpio29 VRgpio
+// 	gpio30 VRgpio
+// 	gpio31 VRgpio
+// 	gpio32 VRgpio
+// 	gpio33 VRgpio
+// }
