2d06f3e5efa819919300a5d4d605148b9040c51a
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-09-19 10:25:01 +0900

    関心の分離がうまくできてなかったとこの修正

--- PLC/processing/gpiooperation/virtualgpio/vrgpiomapping.go
+++ PLC/processing/gpiooperation/virtualgpio/vrgpiomapping.go
@@ -2,7 +2,6 @@ package virtualgpio
 
 import (
 	"PLC/datamodel/vrgpiomodel"
-	"fmt"
 	"strconv"
 )
 
@@ -18,31 +17,4 @@ func VirtualGpioMapping() map[string]*vrgpiomodel.VRgpio {
 	}
 
 	return vrgpio
-}
-
-// virtual gpio の中身を表示する
-// 最初はrangeでやったがうまくいかなかった
-// rangeは乱択であり、かつ表示できる要素数に制限があるのかも
-func ShowVirtualGpio(vrgpio map[string]*vrgpiomodel.VRgpio) {
-	for i := 0 ; i < 34 ; i++ {
-		str := "Gpio" + strconv.Itoa(i)
-		fmt.Printf("%v\t{mode:%v,\tstate:%v}\n",
-			str, vrgpio[str].GpioMode, vrgpio[str].GpioState)
-	}
-}
-
-// 動作チェック用関数
-func CheckVirtualGpio() {
-
-	// gpioが適切にマッピングできてるかチェック
-	fmt.Println("マッピング確認")
-	vrgpio := VirtualGpioMapping()
-	ShowVirtualGpio(vrgpio)
-	fmt.Println()
-
-	// virtual gpio の mode を変更
-	fmt.Println("モード変更確認")
-	VrgpioModeChange("Gpio3", "input", vrgpio)
-	ShowVirtualGpio(vrgpio)
-	fmt.Println()
 }
