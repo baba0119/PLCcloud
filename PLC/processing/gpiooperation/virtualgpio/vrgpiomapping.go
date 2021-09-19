a1ff009e04d9016a5e3cf5436cb4bacdededbd5a
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-09-19 11:40:31 +0900

    一行しか変わらん

--- PLC/processing/gpiooperation/virtualgpio/vrgpiomapping.go
+++ PLC/processing/gpiooperation/virtualgpio/vrgpiomapping.go
@@ -6,6 +6,7 @@ import (
 )
 
 // vrtual gpio のマッピング
+// PLC状態管理レイヤーで実際の処理では扱う
 func VirtualGpioMapping() map[string]*vrgpiomodel.VRgpio {
 	vrgpio := make(map[string]*vrgpiomodel.VRgpio)
 	for i := 0 ; i < 34 ; i++ {
