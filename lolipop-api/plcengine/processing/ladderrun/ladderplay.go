24975942289c0d4b8d4250b1f654fe5d665389be
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-09-24 10:36:49 +0900

    ラダープログラム動作エンジン修正

--- lolipop-api/plcengine/processing/ladderrun/ladderplay.go
+++ lolipop-api/plcengine/processing/ladderrun/ladderplay.go
@@ -29,9 +29,21 @@ func LadderPlay(
 	vrgpio				map[string]*vrgpiomodel.VRgpio,
 	// uniquiOpSlice	*ldexemodel.UniqueOutputStateModel,
 ) bool {
-	// var ldop bool
+	OpStateSliceCache := make(map[string]*ldexemodel.OutputLdexeModel)
+	vrgpioCache := make(map[string]*vrgpiomodel.VRgpio)
+	stateDiff := false
+
+	// キャッシュへマップをコピー
+	opStateMapCopy(OpStateSliceCache, opStateSlice)
+	vrgpioMapCopy(vrgpioCache, vrgpio)
+
 	for {
-		if <-done {
+
+		// 入力があったときにラダープログラム動作
+		// 前回と出力の状態が違うときにも動作
+		// 出力:
+		// opStateSlice と vrgpio が出力
+		if <-done || stateDiff {
 			for _, ld := range inputLdSlice {
 				// ラダープログラム処理 ture or false return
 				outCome, result := BlockInnerProcessing(
@@ -54,6 +66,63 @@ func LadderPlay(
 					return false
 				}
 			}
+
+			// 出力スライスとキャッシュを評価
+			// true -> 差分あり false -> 差分なし
+			stateDiff = opStateComparison(OpStateSliceCache, opStateSlice) || vrgpioComparison(vrgpioCache, vrgpio)
+
+			// 違う場合
+			// キャッシュへ値の代入、繰り返し
+			// キャッシュへマップをコピー
+			opStateMapCopy(OpStateSliceCache, opStateSlice)
+			vrgpioMapCopy(vrgpioCache, vrgpio)
 		}
 	}
+}
+
+// マップのコピー(参照渡しを避ける)
+// 出力保持マップのコピー
+func opStateMapCopy(cache, data map[string]*ldexemodel.OutputLdexeModel) {
+	for key, value := range data {
+		cache[key] = &ldexemodel.OutputLdexeModel{
+			NodeType: value.NodeType,
+			State:    value.State,
+		}
+	}
+}
+
+// virtual gpio のコピー(参照渡しを避ける)
+func vrgpioMapCopy(cache, data map[string]*vrgpiomodel.VRgpio) {
+	for key, value := range data {
+		cache[key] = &vrgpiomodel.VRgpio{
+			GpioMode:  value.GpioMode,
+			GpioState: value.GpioState,
+		}
+	}
+}
+
+// 出力保持マップのキャッシュとの比較
+// 戻り値
+// 差分があったとき -> true
+// 差分がなかった時 -> false
+func opStateComparison(cache, data map[string]*ldexemodel.OutputLdexeModel) bool {
+	for key, value := range data {
+		if cache[key].State != value.State {
+			return true
+		}
+	}
+	return false
+}
+
+// virtual gpio のキャッシュとの比較
+// 戻り値
+// 差分があったとき -> true
+// 差分がなかった時 -> false
+func vrgpioComparison(cache, data map[string]*vrgpiomodel.VRgpio) bool {
+	for key, value := range data {
+		if cache[key].GpioState != value.GpioState {
+			return true
+		}
+	}
+	return false
 }
