3f0b106012ea532108e373c2a78ca10c8baaa91c
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-12-14 18:49:15 +0900

    書き込み追加

--- web-api/controlers/login.go
+++ web-api/controlers/login.go
@@ -1,7 +1,55 @@
 package controlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func Login(w http.ResponseWriter, r *http.Request) {
-	
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	// プリフライトリクエストへ対応
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "application/json" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body := make([]byte, length)
+	_, err = r.Body.Read(body)
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var jsonBody map[string]interface{}
+	err = json.Unmarshal(body, &jsonBody)
+	if err != nil {
+		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
