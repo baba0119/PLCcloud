12a87008e85063d947abfd58fa7090006850e062
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-12-18 08:10:33 +0900

    ログ出力部分の修正

--- web-api/infrastructure/db/newprojectcreate.go
+++ web-api/infrastructure/db/newprojectcreate.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/google/uuid"
 )
@@ -9,28 +9,28 @@ import (
 func NewProjectCreate(userid, projectName string) (bool, error) {
 	u, err := uuid.NewRandom()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return false, err
 	}
 	projectid := u.String()
 
 	u, err = uuid.NewRandom()
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return false, err
 	}
 	plcid := u.String()
 
 	stmtInsert, err := Db.Prepare("INSERT INTO projects(userid, projectid, projectname, plcid) VALUES(?, ?, ?, ?)")
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return false, err
 	}
 	defer stmtInsert.Close()
 
 	_, err = stmtInsert.Exec(userid, projectid, projectName, plcid)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 		return false, err
 	}
 
