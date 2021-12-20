7891b6c573b64382c0a72fc980e9b256847cbfa0
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-12-20 14:29:00 +0900

    プロジェクト名の取得実装

--- GUI/plcgui-ver0.2/component/parts/planefigure/ladderHeader/topMonitor.tsx
+++ GUI/plcgui-ver0.2/component/parts/planefigure/ladderHeader/topMonitor.tsx
@@ -1,5 +1,6 @@
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import styled from "styled-components";
+import { END_POINT } from "../../../../utils/endpoint";
 import SaveButtonPanel from "./detailes/saveButtonPanel";
 
 const AreaParent = styled.div`
@@ -27,10 +28,33 @@ const SaveButtonArea = styled.div`
 `;
 
 const TopMonitor: VFC = () => {
+  const [projectName, setProjectName] = useState("");
+
+  useEffect(() => {
+    const projectid = sessionStorage.getItem("projectid") as string;
+    const endpoint = END_POINT + `/ladder?projectid=${projectid}`;
+    fetch(endpoint, {
+      method: "GET",
+      mode: "cors",
+      credentials: 'include',
+    }).then(res => {
+      if (!res.ok) {
+        console.log("サーバーエラー");
+        return
+      }
+      return res.json()
+    }).then(res => {
+      console.log(res)
+      setProjectName(res.Name);
+    }).catch(error => {
+      console.log("通信失敗");
+    })
+  }, [])
+
   return (
     <AreaParent>
       <ProjectTitleArea>
-        <ProjectTitle>Sample-Ladder</ProjectTitle>
+        <ProjectTitle>{projectName}</ProjectTitle>
       </ProjectTitleArea>
       <SaveButtonArea>
         <SaveButtonPanel/>
--- web-api/infrastructure/db/getprojectname.go
+++ web-api/infrastructure/db/getprojectname.go
@@ -5,7 +5,7 @@ import "log"
 func GetProjectName(projectid string) (string, error) {
 	var projectname string
 	selectStr := "SELECT projectname FROM projects WHERE projectid = ?"
-	err := Db.QueryRow(selectStr, projectid).Scan(&projectid)
+	err := Db.QueryRow(selectStr, projectid).Scan(&projectname)
 	if err != nil {
 		log.Println(err)
 		return "", err
