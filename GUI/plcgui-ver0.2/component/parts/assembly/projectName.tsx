1f36b2cb2246a2174ab30cd9909ba5c61700907f
baba0119 <70310558+baba0119@users.noreply.github.com>
2021-12-28 14:46:10 +0900

    プロジェクトnameの表示まで完了

--- GUI/plcgui-ver0.2/component/parts/assembly/projectName.tsx
+++ GUI/plcgui-ver0.2/component/parts/assembly/projectName.tsx
@@ -1,13 +1,46 @@
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import styled from "styled-components";
+import { END_POINT } from "../../../utils/endpoint";
 
 const AreaParent = styled.div`
   width: 100%;
+  height: 20vh;
+  display: flex;
+  align-items: center;
+`;
+
+const NamePrint = styled.h2`
+  color: #444444;
 `;
 
 const ProjectName: VFC = () => {
+  const [projectName, setProjectName] = useState("");
+
+  useEffect(() => {
+    const projectid = sessionStorage.getItem("projectid") as string;
+    const endpoint = END_POINT + `/ladder?projectid=${projectid}`;
+    fetch(endpoint, {
+      method: "GET",
+      mode: "cors",
+      credentials: 'include',
+    }).then(res => {
+      if (!res.ok) {
+        console.log("サーバーエラー");
+        return
+      }
+      return res.json()
+    }).then(res => {
+      console.log(res)
+      setProjectName(res.Name);
+    }).catch(error => {
+      console.log("通信失敗");
+    })
+  }, [])
+
   return (
-    <AreaParent></AreaParent>
+    <AreaParent>
+      <NamePrint>{projectName}</NamePrint>
+    </AreaParent>
   )
 }
 
