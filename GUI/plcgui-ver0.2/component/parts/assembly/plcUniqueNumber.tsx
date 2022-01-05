c750a9857176e88c975ea2db3f33705ea65bb5d1
baba0119 <70310558+baba0119@users.noreply.github.com>
2022-01-05 14:51:08 +0900

    識別idの取得から表示

--- GUI/plcgui-ver0.2/component/parts/assembly/plcUniqueNumber.tsx
+++ GUI/plcgui-ver0.2/component/parts/assembly/plcUniqueNumber.tsx
@@ -1,20 +1,60 @@
-import { VFC } from "react";
+import { useEffect, useState, VFC } from "react";
 import styled from "styled-components";
+import { END_POINT } from "../../../utils/endpoint";
 import ConsoleDisplayFlame from "../flame/consoleDisplayFlame";
 
 const AreaParent = styled.div`
   width: 100%;
 `;
 
+const PlcIdTextAreaParent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const PlcIdText = styled.p`
+  font-size: 20px;
+  color: #444444;
+`;
+
 const PLCUniqueNumber: VFC = () => {
+  const [plcId, setPlcId] = useState("");
+
   // 一番最初にapiのコール
   // 識別idの取得
+  useEffect(() => {
+    const projectid = sessionStorage.getItem("projectid") as string;
+    const endpoint = END_POINT + `/plcid?projectid=${projectid}`;
+    fetch(endpoint, {
+      method: "GET",
+      mode: "cors",
+      credentials: 'include',
+    }).then(res => {
+      if (!res.ok) {
+        console.log("サーバーエラー");
+        return
+      }
+      return res.text()
+    }).then(res => {
+      console.log(res)
+      setPlcId(res as string);
+    }).catch(error => {
+      console.log("通信失敗");
+    })
+  }, [])
+
   return (
     <AreaParent>
       <ConsoleDisplayFlame
         texts="PLC識別番号"
         Component={() => (
-          <p></p>
+          <PlcIdTextAreaParent>
+            <PlcIdText>
+              {plcId}
+            </PlcIdText>
+          </PlcIdTextAreaParent>
         )}
       />
     </AreaParent>
